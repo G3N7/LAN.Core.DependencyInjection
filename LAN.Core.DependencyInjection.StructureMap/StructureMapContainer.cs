83d8ad30c20c1dba3c009eeb0d922a542e970d8c
Mitch Ferrer <mr.ferfer@gmail.com>
2015-02-14 21:26:30 -0600

    adding StructureMap Implementation

---
+++ LAN.Core.DependencyInjection.StructureMap/StructureMapContainer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using StructureMap.Diagnostics.TreeView;
+using IStructureMapContainer = StructureMap.IContainer;
+
+namespace LAN.Core.DependencyInjection.StructureMap
+{
+	public class StructureMapContainer : IContainer
+	{
+		private readonly IStructureMapContainer _container;
+
+		public StructureMapContainer(IStructureMapContainer container)
+		{
+			_container = container;
+		}
+
+		public void Bind<TAbstract, TConcrete>(bool singleton)
+			where TAbstract : class
+			where TConcrete : class, TAbstract
+		{
+			_container.Configure(x =>
+					{
+						var binding = x.For<TAbstract>().Use<TConcrete>();
+						if (singleton)
+						{
+							binding.Singleton();
+						}
+						else
+						{
+							binding.AlwaysUnique();
+						}
+					}
+				);
+		}
+
+		public TAbstract GetInstance<TAbstract>() where TAbstract : class
+		{
+			return _container.GetInstance<TAbstract>();
+		}
+
+		public object GetInstance(Type abstractType)
+		{
+			return _container.GetInstance(abstractType);
+		}
+
+		public void RegisterSingleton<TAbstract>(TAbstract singletonOfConcrete) where TAbstract : class
+		{
+			_container.Configure(x => x.For<TAbstract>().Use(singletonOfConcrete));
+		}
+
+		public RegistrationResult AreAllRequiredDependenciesRegistered()
+		{
+			try
+			{
+				this._container.AssertConfigurationIsValid();
+				return new RegistrationResult("Success", false);
+			}
+			catch (Exception ex)
+			{
+				return new RegistrationResult(ex.ToString(), true);
+			}
+		}
+	}
+}
---
+++ LAN.Core.DependencyInjection.Tests/StructureMapContainer.cs
@@ -0,0 +1,13 @@
+﻿using LAN.Core.DependencyInjection.StructureMap;
+using StructureMap;
+
+namespace LAN.Core.DependencyInjection.Tests
+{
+	public class StructureMapContainerTests : ContainersMustSatisfy<StructureMapContainer>
+	{
+		protected override StructureMapContainer CreateContainer()
+		{
+			return new StructureMapContainer(new Container());
+		}
+	}
+}
