780d9cbe3775d6fd9f8ae6188e06100c0acaf7c8
Mitch Ferrer <mr.ferfer@gmail.com>
2015-04-21 09:40:44 -0700

    fixing nuget build script to use release mode, and output symbols

--- NugetBuild.cmd
+++ NugetBuild.cmd
@@ -1,19 +1,19 @@
 @echo off
 
 @echo !! Rebuild in Release mode prior to running !!
-.nuget\nuget pack LAN.Core.DependencyInjection\LAN.Core.DependencyInjection.csproj -IncludeReferencedProjects -ExcludeEmptyDirectories
+.nuget\nuget pack LAN.Core.DependencyInjection\LAN.Core.DependencyInjection.csproj -IncludeReferencedProjects -ExcludeEmptyDirectories -Build -Symbols -Properties Configuration=Release
 @echo Finished Building: LAN.Core.DependencyInjection
 
 @echo ---------------------------------------------------
-.nuget\nuget pack LAN.Core.DependencyInjection.Ninject\LAN.Core.DependencyInjection.Ninject.csproj -IncludeReferencedProjects -ExcludeEmptyDirectories
+.nuget\nuget pack LAN.Core.DependencyInjection.Ninject\LAN.Core.DependencyInjection.Ninject.csproj -IncludeReferencedProjects -ExcludeEmptyDirectories -Build -Symbols -Properties Configuration=Release
 @echo Finished Building: LAN.Core.DependencyInjection.Ninject
 
 @echo ---------------------------------------------------
-.nuget\nuget pack LAN.Core.DependencyInjection.SimpleInjector\LAN.Core.DependencyInjection.SimpleInjector.csproj -IncludeReferencedProjects -ExcludeEmptyDirectories
+.nuget\nuget pack LAN.Core.DependencyInjection.SimpleInjector\LAN.Core.DependencyInjection.SimpleInjector.csproj -IncludeReferencedProjects -ExcludeEmptyDirectories -Build -Symbols -Properties Configuration=Release
 @echo Finished Building: LAN.Core.DependencyInjection.SimpleInjector
 
 @echo ---------------------------------------------------
-.nuget\nuget pack LAN.Core.DependencyInjection.StructureMap\LAN.Core.DependencyInjection.StructureMap.csproj -IncludeReferencedProjects -ExcludeEmptyDirectories
+.nuget\nuget pack LAN.Core.DependencyInjection.StructureMap\LAN.Core.DependencyInjection.StructureMap.csproj -IncludeReferencedProjects -ExcludeEmptyDirectories -Build -Symbols -Properties Configuration=Release
 @echo Finished Building: LAN.Core.DependencyInjection.StructureMap
 
 @echo ---------------------------------------------------
