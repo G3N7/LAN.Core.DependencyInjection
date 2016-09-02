7caea8a145a5fd210bc755920454b9e0eaa6b1e6
Mitch Ferrer <mr.ferfer@gmail.com>
2016-09-02 13:09:44 -0600

    updating nuget for publications

--- NugetBuildStructureMap.cmd
+++ NugetBuildStructureMap.cmd
@@ -1,8 +1,8 @@
 @echo off
 
 set /p apiKey=Enter API Key:%=%
-.nuget\nuget setApiKey %apiKey%
-
+.nuget\nuget setApiKey %apiKey% -Source https://www.nuget.org/api/v2/package
+ 
 @echo ---------------------------------------------------
 .nuget\nuget pack LAN.Core.DependencyInjection.StructureMap\LAN.Core.DependencyInjection.StructureMap.csproj -IncludeReferencedProjects -ExcludeEmptyDirectories -Build -Symbols -Properties Configuration=Release
 @echo Finished Building: LAN.Core.DependencyInjection.StructureMap
@@ -10,5 +10,5 @@ set /p apiKey=Enter API Key:%=%
 
 @echo Pushing To Nuget and SymbolSource...
 set /p diStructureMapVersion=Enter StructureMap Package Version:%=%
-.nuget\nuget push LAN.Core.DependencyInjection.StructureMap.%diStructureMapVersion%.nupkg
+.nuget\nuget push LAN.Core.DependencyInjection.StructureMap.%diStructureMapVersion%.nupkg -Source https://www.nuget.org/api/v2/package
 @echo ---------------------------------------------------
