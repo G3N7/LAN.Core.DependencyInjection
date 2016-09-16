4c7258357f88f4367ad37edc1ef173af33d7f449
Mitch Ferrer <mr.ferfer@gmail.com>
2016-09-15 20:25:38 -0600

    setting source for nuget

--- NugetBuildSimpleInjector.cmd
+++ NugetBuildSimpleInjector.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 set /p apiKey=Enter API Key:%=%
-.nuget\nuget setApiKey %apiKey%
+.nuget\nuget setApiKey %apiKey% -Source https://www.nuget.org/api/v2/package
 
 @echo ---------------------------------------------------
 .nuget\nuget pack LAN.Core.DependencyInjection.SimpleInjector\LAN.Core.DependencyInjection.SimpleInjector.csproj -IncludeReferencedProjects -ExcludeEmptyDirectories -Build -Symbols -Properties Configuration=Release
@@ -10,5 +10,5 @@ set /p apiKey=Enter API Key:%=%
 
 @echo Pushing To Nuget and SymbolSource...
 set /p diSimpleInjectorVersion=Enter SimpleInjector Package Version:%=%
-.nuget\nuget push LAN.Core.DependencyInjection.SimpleInjector.%diSimpleInjectorVersion%.nupkg
+.nuget\nuget push LAN.Core.DependencyInjection.SimpleInjector.%diSimpleInjectorVersion%.nupkg -Source https://www.nuget.org/api/v2/package
 @echo ---------------------------------------------------
